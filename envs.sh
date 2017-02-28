6216033d5593f48103f31169ac930195684d0c0c
Yaseen Kadir <yaseen@kadaring.com>
2017-02-28 22:18:49 +1100

    Added envs.sh to make it easier to edit/source environment variables

---
+++ envs.sh
@@ -0,0 +1,11 @@
+export TIMETABLE_BOT_USERNAME="{student number}"
+export TIMETABLE_BOT_PASSWORD="{timetable password}"
+export TIMETABLE_BOT_SUBJECT="{subject code}"
+export TIMETABLE_BOT_CLASS_TYPE="{class type}"
+export TIMETABLE_BOT_ACTIVITY_CODES="{class activity codes as comma separated list}"
+export TIMETABLE_BOT_TO_EMAIL="{notify email address}"
+export TIMETABLE_BOT_PING_FREQUENCY="10"  # Optional value to set a ping frequency.
+export GMAIL_USERNAME="{sender email address}"
+export GMAIL_PASSWORD="{sender email password}"
+export GMAIL_SMTP_SERVER="smtp.gmail.com"   # Change smtp server details if using another email provider.
+export GMAIL_SMTP_SERVER_PORT="587"
