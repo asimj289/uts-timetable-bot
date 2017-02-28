3738e3e6fc282c747bfa80be14a161d3a5524e5b
Yaseen Kadir <yaseen@kadaring.com>
2017-02-28 22:40:31 +1100

    Improved descriptions and added comments about environment variables

--- envs.sh
+++ envs.sh
@@ -1,9 +1,11 @@
+# When setting values replace, make sure to remove the curly braces too.
 export TIMETABLE_BOT_USERNAME="{student number}"
-export TIMETABLE_BOT_PASSWORD="{timetable password}"
+export TIMETABLE_BOT_PASSWORD="{password}"
 export TIMETABLE_BOT_SUBJECT="{subject code}"
 export TIMETABLE_BOT_CLASS_TYPE="{class type}"
-export TIMETABLE_BOT_ACTIVITY_CODES="{class activity codes as comma separated list}"
-export TIMETABLE_BOT_TO_EMAIL="{notify email address}"
+# Commas not required for single class e.g. 01
+export TIMETABLE_BOT_ACTIVITY_CODES="{class activity codes, comma separated e.g. 01,02. Leading zeros required}"
+export TIMETABLE_BOT_TO_EMAIL="{email address to notify}"
 export TIMETABLE_BOT_PING_FREQUENCY="10"  # Optional value to set a ping frequency.
 export GMAIL_USERNAME="{sender email address}"
 export GMAIL_PASSWORD="{sender email password}"
